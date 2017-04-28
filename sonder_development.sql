0cde58f934e5331adf0dde78199ee48e94e51604
seqs <hi@seqs.co>
2017-04-28 22:08:36 +0800

    update

--- sonder_development.sql
+++ sonder_development.sql
@@ -1,29 +1,239 @@
--- --------------------------------------------------------
--- 主机:                           127.0.0.1
--- 服务器版本:                        10.1.19-MariaDB - mariadb.org binary distribution
--- 服务器操作系统:                      Win32
--- HeidiSQL 版本:                  9.4.0.5125
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
--- 数据导出被取消选择。
+DROP DATABASE IF EXISTS `sonder_development`;
+CREATE DATABASE IF NOT EXISTS `sonder_development` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `sonder_development`;
+
+DROP TABLE IF EXISTS `apikeys`;
+CREATE TABLE IF NOT EXISTS `apikeys` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(255) DEFAULT NULL,
+  `secret` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` text,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `groups_user`;
+CREATE TABLE IF NOT EXISTS `groups_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `ims_hticket_activity`;
+CREATE TABLE IF NOT EXISTS `ims_hticket_activity` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uniacid` int(10) unsigned NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `shareimg` varchar(100) NOT NULL,
+  `singleimg` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `status` tinyint(1) unsigned DEFAULT '0',
+  `starttime` int(11) unsigned NOT NULL,
+  `endtime` int(11) unsigned NOT NULL,
+  `place` varchar(100) NOT NULL,
+  `createtime` int(11) unsigned NOT NULL,
+  `extype` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `proname` varchar(50) NOT NULL,
+  `tlimit` int(11) unsigned NOT NULL,
+  `scantimes` int(11) unsigned NOT NULL DEFAULT '1',
+  `buylimit` int(8) unsigned NOT NULL,
+  `fee` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `author` varchar(50) NOT NULL,
+  `groups` varchar(200) NOT NULL,
+  `viewnums` int(11) unsigned NOT NULL,
+  `isseat` tinyint(2) unsigned NOT NULL,
+  `seatsets` text NOT NULL,
+  `usedseats` text NOT NULL,
+  `orderseats` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uniacid` (`uniacid`),
+  KEY `status` (`status`),
+  KEY `isseat` (`isseat`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `ims_hticket_log`;
+CREATE TABLE IF NOT EXISTS `ims_hticket_log` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uniacid` int(10) unsigned NOT NULL,
+  `orderid` int(11) unsigned NOT NULL,
+  `actid` int(11) unsigned NOT NULL,
+  `scanown` varchar(50) NOT NULL,
+  `type` int(2) unsigned NOT NULL DEFAULT '1',
+  `remark` varchar(100) NOT NULL,
+  `createtime` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uniacid` (`uniacid`),
+  KEY `orderid` (`orderid`),
+  KEY `actid` (`actid`),
+  KEY `scanown` (`scanown`),
+  KEY `type` (`type`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `ims_hticket_order`;
+CREATE TABLE IF NOT EXISTS `ims_hticket_order` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uniacid` int(10) unsigned NOT NULL,
+  `openid` varchar(50) NOT NULL,
+  `actid` int(11) unsigned NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `fee` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `status` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `scanown` varchar(50) NOT NULL,
+  `remark` varchar(50) NOT NULL,
+  `createtime` int(11) unsigned NOT NULL,
+  `paytime` int(11) unsigned NOT NULL,
+  `scantime` int(11) unsigned NOT NULL,
+  `closetime` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uniacid` (`uniacid`),
+  KEY `openid` (`openid`),
+  KEY `actid` (`actid`),
+  KEY `status` (`status`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `ims_hticket_seat`;
+CREATE TABLE IF NOT EXISTS `ims_hticket_seat` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uniacid` int(10) unsigned NOT NULL,
+  `openid` varchar(50) NOT NULL,
+  `orid` int(11) unsigned NOT NULL,
+  `seats` text NOT NULL,
+  `ptime` varchar(50) NOT NULL,
+  `nums` int(11) unsigned NOT NULL DEFAULT '0',
+  `price` float NOT NULL,
+  `createtime` int(11) unsigned NOT NULL,
+  `remark` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uniacid` (`uniacid`),
+  KEY `openid` (`openid`),
+  KEY `orid` (`orid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `content` text,
+  `image` varchar(255) DEFAULT NULL,
+  `featured` int(11) DEFAULT NULL,
+  `page` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  `meta_title` varchar(255) DEFAULT NULL,
+  `meta_description` text,
+  `author_id` int(11) DEFAULT NULL,
+  `group_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `published_at` datetime DEFAULT NULL,
+  `published_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `posts_tag`;
+CREATE TABLE IF NOT EXISTS `posts_tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) DEFAULT NULL,
+  `tag_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) DEFAULT NULL,
+  `key` varchar(255) DEFAULT NULL,
+  `value` text,
+  `type` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `description` text,
+  `parent_id` int(11) DEFAULT NULL,
+  `meta_title` varchar(255) DEFAULT NULL,
+  `meta_description` text,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `tokens`;
+CREATE TABLE IF NOT EXISTS `tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `ip` varchar(255) DEFAULT NULL,
+  `lifetime` int(11) DEFAULT NULL,
+  `is_revoked` tinyint(1) DEFAULT NULL,
+  `last_activity` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) DEFAULT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `cover` varchar(255) DEFAULT NULL,
+  `bio` text,
+  `website` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `accessibility` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  `meta_title` varchar(255) DEFAULT NULL,
+  `meta_description` text,
+  `last_login` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
